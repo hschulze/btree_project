372e1bfed1376d353ba7b767fa1df197568af74f
cloar <cloar@2f5b6336-ee6c-bd46-a91d-a076b18e44cd>
2012-04-26 20:52:47 +0000

    delete funzt fast...immer noch nicht richtig
    git-svn-id: http://192.168.247.105:8888/svn/java_projects/trunk@50 2f5b6336-ee6c-bd46-a91d-a076b18e44cd

--- bbaeume/src/franz/tree/BTree.java
+++ bbaeume/src/franz/tree/BTree.java
@@ -116,6 +116,65 @@ public class BTree {
 		numberOfTreeEntrys--;
 		return result;
 	}
+	
+	private NodeEntry removeKey2(BNode node, int key) {
+		// get information needed
+		BNode delBTNode = searchKey(key).getNode();
+		if (delBTNode == null) {
+			return null;
+		}
+		int keyIndex = delBTNode.containsKey(key);
+		BNode returnNode = delBTNode.getKey(keyIndex);
+
+
+		if (delBTNode.getNumberOfChilds() == 0) {				// is a leaf **********************************
+			if (delBTNode.nKey > order - 1)
+				// we can delete KeyNode directly
+			{
+				delBTNode.extractKeyNode(keyIndex);
+			} else
+				// we need to get more keys so that we can delete it
+			{
+				BNode parentBTNode = delBTNode.parent;
+				int parentIndex = 0;
+				while (parentBTNode.getBTNode(parentIndex) != delBTNode)
+					parentIndex++;
+				if (parentIndex == parentBTNode.nKey) {
+					BNode leftBTNode = parentBTNode.getBTNode(parentIndex - 1);
+					if (leftBTNode.nKey > order - 1) {
+						delBTNode.kArray[keyIndex] = parentBTNode.getKeyNode(parentIndex);
+						parentBTNode.kArray[parentIndex] = leftBTNode.getKeyNode(leftBTNode.nKey - 1);
+						deleteNode(leftBTNode, leftBTNode.getKeyNode(leftBTNode.nKey - 1).getKey());
+					} else {
+						delBTNode.mergeWithBTNode();
+					}
+				} else {
+					BNode rightBTNode = parentBTNode.getBTNode(parentIndex + 1);
+					if (rightBTNode.nKey > order - 1) {
+						delBTNode.kArray[keyIndex] = parentBTNode.getKeyNode(parentIndex);
+						parentBTNode.kArray[parentIndex] = rightBTNode.getKeyNode(0);
+						deleteNode(rightBTNode, rightBTNode.getKeyNode(0).getKey());
+					} else {
+						delBTNode.mergeWithBTNode();
+					}
+				}
+			}
+		} else { // is internal node *********************
+			//try predecesor
+			// get the node to exchange and delete it at leaf position
+			BNode preBTNode = delBTNode.getBTNode(keyIndex);
+			while (preBTNode.getNumberOfChilds() > 0) {
+				preBTNode = preBTNode.getBTNode(preBTNode.nKey);
+			}
+
+			// swap nodes
+			BNode tmpKeyNode = preBTNode.getKeyNode(preBTNode.nKey - 1);
+			preBTNode.kArray[preBTNode.nKey - 1] = delBTNode.kArray[keyIndex];
+			delBTNode.kArray[keyIndex] = tmpKeyNode;
+			deleteNode(preBTNode, preBTNode.getKeyNode(preBTNode.nKey - 1).getKey());
+		}
+		return returnNode;
+	}
 	/**
 	 * Funktion geht davon aus, dass node oder seine Unterbaeume den Schluessel enthalten
 	 * @param node
@@ -131,6 +190,8 @@ public class BTree {
 		if(keyEntryPosition < 0) {										// Knoten enthaelt Key NICHT
 			
 			int nextChildPosition = node.getNextChildPositionForKey(key);
+			
+			
 			returnValue = removeKey(node.getChild(nextChildPosition), key);
 			
 			// Fall 1: nextChild besitzt nach dem Loeschen noch m Schluessel => fertig (167)
